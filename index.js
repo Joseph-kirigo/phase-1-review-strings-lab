c0295d50dcb6844c4eb204ccd92ccb328e533bac
Joseph-kirigo <josephkirigo777@gmail.com>
2022-08-20 07:41:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your code in this file!
+
+const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = 'Welcome to Flatbook, ';
+
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser;
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const currentUser = 'Grace Hopper';
+
+// const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// const shortGreeting = 'Welcome, ';
+
+// const shortGreeting = `Welcome, ${currentUser}`;
+
+// const shortGreeting = `Welcome, ${currentUser}!`;
+
+const shortGreeting=`Welcome, ${currentUser.slice(0,-4)}!`
